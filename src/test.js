c8a429e1fdb9cc8cc361b30a0a494273dd98bbca
Paul Trampert <paul.trampert@gmail.com>
2017-11-09 21:54:27 -0500

    Initial commit/project setup

---
+++ src/test.js
@@ -0,0 +1,8 @@
+import 'babel-polyfill';
+import {configure} from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({adapter: new Adapter()});
+
+let ctx = require.context('./', true, /\.spec.jsx?$/);
+ctx.keys().forEach(ctx);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const webpack = require('webpack');
+const path = require('path');
+const {dependencies} = require('./package.json');
+
+const SRC = path.resolve(__dirname, 'src');
+const DIST = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  devtool: 'sourcemap',
+  stats: 'errors-only',
+  entry: {
+    'react-validator': path.resolve(SRC, 'index.js')
+  },
+  output: {
+    path: DIST,
+    filename: '[name].js',
+    library: 'reactValidator',
+    libraryTarget: 'umd'
+  },
+  externals: Object.keys(dependencies),
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  },
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        include: SRC,
+        loader: 'eslint-loader',
+        options: {
+          fix: true
+        }
+      },
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader'
+        ]
+      }
+    ]
+  }
+};
