232723c6fc3c04e79293a241696714607a5a8fd3
Paul Trampert <paul.trampert@gmail.com>
2018-03-10 00:42:42 -0500

    Clean up index.js

---
+++ src/modules.js
@@ -0,0 +1,4 @@
+export {default as Form} from './Form';
+export {default as FormContext} from './FormContext';
+export {default as formContextAware} from './formContextAware';
+export {default as withValidation} from './withValidation';
